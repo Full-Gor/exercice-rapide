f99c6cafaa95bebef7d11234ae2c9c1e4a7088a3
Full-Gor <full-gor@github.com>
2025-09-19 15:19:35 +0200

    add simple calculatrice

--- calculatrice/calculatrice.rb
+++ calculatrice/calculatrice.rb
@@ -26,34 +26,5 @@ puts "#{nombre1} / #{nombre2} = #{division}"
 puts ""
 puts "Merci d'avoir utilisé la calculatrice !"
 
-#__________________________
-# VERSION JAVASCRIPT (pour comparaison)
-# calculatrice.js
-
-console.log("=== CALCULATRICE SIMPLE ===");
-console.log("");
-
-// Demander les nombres
-console.log("Entrez le premier nombre :");
-const nombre1 = parseFloat(prompt("Premier nombre"));
-
-console.log("Entrez le deuxième nombre :");
-const nombre2 = parseFloat(prompt("Deuxième nombre"));
-
-console.log("");
-console.log("=== RESULTATS ===");
-
-// Calculs directs - pas de if !
-const addition = nombre1 + nombre2;
-const soustraction = nombre1 - nombre2;
-const multiplication = nombre1 * nombre2;
-const division = nombre1 / nombre2;
-
-console.log(`${nombre1} + ${nombre2} = ${addition}`);
-console.log(`${nombre1} - ${nombre2} = ${soustraction}`);
-console.log(`${nombre1} * ${nombre2} = ${multiplication}`);
-console.log(`${nombre1} / ${nombre2} = ${division}`);
-
-console.log("");
-console.log("Merci d'avoir utilisé la calculatrice !");
+puts'
 
